cde95654db4e83a483e34124d13100d1f7414e7f
malika.omarova.r@gmail.com <malika.omarova.r@gmail.com>
2025-10-29 12:30:59 +0600

    Обновлён дизайн и добавлен интерфейс админа

--- tkrpo/settings.py
+++ tkrpo/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # если у тебя есть папка static в проекте
+]
+
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
