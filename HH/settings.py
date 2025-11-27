13c819621f1e08b8bceaa68cb73b17dc5a142178
Aijamal77 <damiraelayum@gmail.com>
2025-11-28 00:49:12 +0600

    Работа над дизайном

--- HH/settings.py
+++ HH/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'head_hunter',
-        'USER': 'root',
-        'PASSWORD': '1234',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '3306',
     }
